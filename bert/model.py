5b9bd4d1cdea97caf32c4bc5221ab30567e03946
서성빈 <2015004148@hanyang.ac.kr>
2021-06-02 21:04:39 +0900

    add preprocessing

---
+++ bert/model.py
@@ -0,0 +1,52 @@
+import numpy as np
+import
+
+""" sinusoid position embedding """
+def get_sinusoid_encoding_table(n_seq, d_hidn):
+    def cal_angle(position, i_hidn):
+        return position / np.power(10000, 2 * (i_hidn // 2) / d_hidn)
+    def get_posi_angle_vec(position):
+        return [cal_angle(position, i_hidn) for i_hidn in range(d_hidn)]
+
+    sinusoid_table = np.array([get_posi_angle_vec(i_seq) for i_seq in range(n_seq)])
+    sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])  # even index sin
+    sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])  # odd index cos
+
+    return sinusoid_table
+
+""" scale dot product attention """
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self, d_head):
+        super().__init__()
+        self.scale = 1 / (d_head ** 0.5)
+
+    def forward(self, Q, K, V, attn_mask):
+        # (bs, n_head, n_q_seq, n_k_seq)
+        scores = torch.matmul(Q, K.transpose(-1, -2)).mul_(self.scale)
+        scores.masked_fill_(attn_mask, -1e9)
+        # (bs, n_head, n_q_seq, n_k_seq)
+        attn_prob = nn.Softmax(dim=-1)(scores)
+        # (bs, n_head, n_q_seq, d_v)
+        context = torch.matmul(attn_prob, V)
+        # (bs, n_head, n_q_seq, d_v), (bs, n_head, n_q_seq, n_v_seq)
+        return context, attn_prob
+
+class EncoderLayer(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.self_attn = MultiHeadAttention(self.config)
+        self.layer_norm1 = nn.LayerNorm(self.config.d_hidn, eps=self.config.layer_norm_epsilon)
+        self.pos_ffn = PoswiseFeedForwardNet(self.config)
+        self.layer_norm2 = nn.LayerNorm(self.config.d_hidn, eps=self.config.layer_norm_epsilon)
+
+    def forward(self, inputs, attn_mask):
+        # (bs, n_enc_seq, d_hidn), (bs, n_head, n_enc_seq, n_enc_seq)
+        att_outputs, attn_prob = self.self_attn(inputs, inputs, inputs, attn_mask)
+        att_outputs = self.layer_norm1(inputs + att_outputs)
+        # (bs, n_enc_seq, d_hidn)
+        ffn_outputs = self.pos_ffn(att_outputs)
+        ffn_outputs = self.layer_norm2(ffn_outputs + att_outputs)
+        # (bs, n_enc_seq, d_hidn), (bs, n_head, n_enc_seq, n_enc_seq)
+        return ffn_outputs, attn_prob
